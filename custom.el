0aeba84ddb2933f5bca6eb5e3ad11c809378f74a
efenyao <felix.yao@ericsson.com>
2015-01-20 17:13:39 +0100

    Bugfix: Default recent file location and defaut font size

--- custom.el
+++ custom.el
@@ -8,12 +8,5 @@
  '(custom-enabled-themes (quote (adwaita)))
  '(global-mark-ring-max 200)
  '(recentf-mode t)
- '(recentf-save-file "~\\.emacs.d\\auto\\recent_file")
+ '(recentf-save-file "~/emacs.d/auto/recent_file")
  '(show-paren-mode t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
