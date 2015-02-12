2c085384e13175619f621e73a3c8bb3d42739ba5
efenyao <felix.yao@ericsson.com>
2015-02-12 18:00:24 +0100

    Add personal blogs

--- modes/org/org-projects.el
+++ modes/org/org-projects.el
@@ -24,18 +24,31 @@
      :base-directory ,(expand-file-name "personal" fy/source-folder)
      :base-extension "org"
      :publishing-directory ,(expand-file-name "personal" fy/publich-folder)
-     :recursive t
+     :recursive f
      :publishing-function org-html-publish-to-html
      :headline-levels 4 
      :auto-preamble t
      )
+    ("org-personal-blog"
+     :base-directory ,(expand-file-name "personal/blog" fy/source-folder)
+     :base-extension "org"
+     :publishing-directory ,(expand-file-name "personal/blog" fy/publich-folder)
+     :recursive t
+     :publishing-function org-html-publish-to-html
+     :auto-sitemap t
+     :sitemap-filename  "blog_sitemap.org"
+     :sitemap-title "Blogs"
+     :sitemap-sort-files anti-chronologically
+     :sitemap-file-entry-format "%t %d"
+    )
     ("org-personal-static"
-     :base-directory ,(expand-file-name "personal" fy/source-folder)
+     :base-directory ,(expand-file-name "personal/img" fy/source-folder)
      :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-     :publishing-directory ,(expand-file-name "personal" fy/publich-folder)
+     :publishing-directory ,(expand-file-name "personal/img" fy/publich-folder)
      :recursive t
      :publishing-function org-publish-attachment
     )
+  
 
     ,(if (file-exists-p (expand-file-name "work" fy/source-folder))
 	 `("org-work"
@@ -49,10 +62,10 @@
 	   ))
 
     ,(if (file-exists-p (expand-file-name "work" fy/source-folder))
-      `("org-work"
-         :base-directory ,(expand-file-name "work" fy/source-folder)
+      `("org-work-static"
+         :base-directory ,(expand-file-name "work/img" fy/source-folder)
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory ,(expand-file-name "work" fy/publich-folder)
+         :publishing-directory ,(expand-file-name "work/img" fy/publich-folder)
          :recursive t
          :publishing-function org-publish-attachment
        ))))
