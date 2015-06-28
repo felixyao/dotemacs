cc0e979a2100297af47f9872c0db019b1c98a985
efenyao <felix.yao@ericsson.com>
2015-06-28 18:33:16 +0200

    Set default space as tab

--- modes/common-c/common.el
+++ modes/common-c/common.el
@@ -1,6 +1,6 @@
 ;basic setting for python c++ and c mode
 (setq-default c-basic-offset 4 c-default-style "linux")
-(setq-default tab-width 4 indent-tabs-mode t)
+(setq-default tab-width 4 indent-tabs-mode nil)
 
 (require 'auto-complete-clang)
 (require 'auto-complete-c-headers)
