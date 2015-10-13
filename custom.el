61a95f70fe4d7a855d95eb166a5b40be9e6b4ad3
efenyao <felix.yao@ericsson.com>
2015-10-13 09:50:31 +0200

    Change theme again

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(cua-enable-cua-keys nil)
- '(custom-enabled-themes (quote (manoj-dark)))
+ '(custom-enabled-themes (quote (misterioso)))
  '(global-mark-ring-max 200)
  '(recentf-mode t)
  '(recentf-save-file "~/emacs.d/auto/recent_file")
