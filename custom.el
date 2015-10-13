d3bcb4e93a8b0a66702aa5f1543b9799a88fbbe7
efenyao <felix.yao@ericsson.com>
2015-10-13 09:44:45 +0200

    Change default theme

--- custom.el
+++ custom.el
@@ -3,8 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(cua-enable-cua-keys nil)
+ '(custom-enabled-themes (quote (manoj-dark)))
  '(global-mark-ring-max 200)
  '(recentf-mode t)
  '(recentf-save-file "~/emacs.d/auto/recent_file")
  '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
